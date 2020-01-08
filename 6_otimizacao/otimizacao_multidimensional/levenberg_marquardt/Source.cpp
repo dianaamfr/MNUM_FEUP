f39ea92d3c3d49e9ed873769cb185c05c2cddc09
Diana Freitas <diana.amfreitas@gmail.com>
2020-01-08 18:42:14 +0000

    levenberg  - tentativa

--- 6_otimizacao/otimizacao_multidimensional/levenberg_marquardt/Source.cpp
+++ 6_otimizacao/otimizacao_multidimensional/levenberg_marquardt/Source.cpp
@@ -8,7 +8,7 @@ using namespace std;
 //double  f(double x, double y) { return sin(y)+pow(y,2)/4 + cos(x)+pow(x,2)/4-1; }
 //double dfx(double x, double y) { return x / 2 - sin(x);}
 //double dfy(double x, double y) { return cos(y) + y / 2; }
-//double  invHxdelta(double x, double y) { return (x / 2 - sin(x)) / (1 / 2 - cos(x)); }
+//double  invHxdelta(double x, double y) { return (x / 2 - sin(x)) / (1 /2 - cos(x)); }
 //double  invHydelta(double x, double y) { return (cos(y) + y / 2) / (1 / 2 - sin(y)); }
 
 //Exercise 2
@@ -20,47 +20,42 @@ double  invHydelta(double x, double y) { return y-4; }
 
 
 
-void levenberg_marquardt(double x0, double y0, double lambda, const double error, int n) {
+void levenberg_marquardt(double x0, double y0, double lambda, int n) {
 
-  double x = x0, y = y0, xn, yn, xant, yant;
-  unsigned int counter = 0;
-  bool not_valid = true;
+  double x = x0, y = y0, xant, yant;
 
-  do {
-    if (counter >= n) break;
-
+  for (unsigned int i = 0; i < n; i++) {
     xant = x;
     yant = y;
-    xn = x - (invHxdelta(x, y)) - (lambda * dfx(x, y));
-    yn = y - (invHydelta(x, y)) - (lambda * dfy(x, y));
 
-    if (f(xn, yn) < f(x, y)) {
+    x = x - invHxdelta(x, y) - lambda * dfx(x, y);
+    y = y - invHydelta(x, y) - lambda * dfy(x, y);
+
+    if (f(x, y) < f(xant, yant)) {
       lambda /= 2;
-      x = xn;
-      y = yn;
-      not_valid = false;
     }
     else {
       lambda *= 2;
-      not_valid = true;
+      x = xant;
+      y = yant;
     }
-    cout << setw(2) << counter << "     " << "f( " << x << " , " << y << " ) = " << f(x, y) << endl;
-    counter++;
-  } while ((abs(xn - xant) > error) || (abs(yn - yant) > error) || not_valid);
+
+    cout << setw(2) << i << "     " << "f( " << x << " , " << y << " ) = " << f(x, y) << endl;
+  }
 }
 
 
 int main() {
-  double x0, y0, prec=pow(10,-3);
+  double x0, y0;
   //Exercise 1 - not done yet
   /*x0=1;
   y0=1;
-  levenberg_marquardt(x0, y0, 0.1, prec, 22);*/
+  levenberg_marquardt(x0, y0, 0.1, 13);*/
 
   //Exercise 2
   x0=0;
   y0=0;
-  levenberg_marquardt(x0, y0, 0.1, prec,4);
+  levenberg_marquardt(x0, y0, 0.1,4);
 
   return 0;
 }
